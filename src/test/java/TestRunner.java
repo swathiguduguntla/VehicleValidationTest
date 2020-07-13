45c986d7dd45e1d1cc8cd9cb3ae2a7c98541533b
swathiguduguntla <46331341+swathiguduguntla@users.noreply.github.com>
2020-07-13 23:30:33 +0100

    Update TestRunner.java

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,11 +1,13 @@
-import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.junit.Cucumber;
 import io.cucumber.testng.CucumberOptions;
+import org.junit.runner.RunWith;
 
+@RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"html:target/carCheck-report.html", "json:target/cucumber.json"},
         features = {"src/test/resources/features"},
         glue = {"com.car.qa.stepDefs"}
 )
-public class TestRunner extends AbstractTestNGCucumberTests {
+public class TestRunner {
 
 }
